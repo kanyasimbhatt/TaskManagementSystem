0f8103b361f575fef9d0f843e45d89b1777a500b
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-03-06 10:52:56 +0530

    add: add edit functionality and modified the search functionality to work synchronously with the filter

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ let colormap = new Map();
 colormap.set("ToDo", "todocolor");
 colormap.set("In Progress", "inprogresscolor");
 colormap.set("Done", "donecolor");
-document.getElementsByClassName("task")[0].value = "";
-document.getElementsByClassName("description")[0].value = "";
 document.getElementById("searching-title").value = "";
 document.getElementById("searching-des").value = "";
 document.getElementById("searching-title-des").value = "";
+
+//common elements
+const addTaskText = document.getElementsByClassName("add-task-text")[0];
+const addButton = document.getElementsByClassName("add-button")[0];
+let title = document.getElementsByClassName("task")[0];
+let description = document.getElementsByClassName("description")[0];
+let status = document.getElementsByClassName("status")[0];
+let statusOption1 = status.children[1];
+let statusOtion2 = status.children[2];
+
+title.value = "";
+description.value = "";
 
 const all_tasks = document.getElementsByClassName("all-tasks")[0];
 
@@ -35,7 +45,7 @@ function addTask() {
   localStorage.setItem("tasks-array", JSON.stringify(arr));
   title.value = "";
   description.value = "";
-  showTasks();
+  showTasks(arr);
 }
 
 function calculate_unselected(status) {
@@ -48,73 +58,107 @@ function calculate_unselected(status) {
   return unselected;
 }
 
-function showTasks() {
-  let tasks = JSON.parse(localStorage.getItem("tasks-array"));
-
+function showTasks(tasks) {
   let htmlcode = "";
 
   for (let a in tasks) {
     let color = colormap.get(tasks[a].status);
     let unselected_options = calculate_unselected(tasks[a].status);
 
-    htmlcode += `<div class="card m-3 ${color}" style="width: 18rem">
+    htmlcode += `<div class="card m-3 ${color} shadow-sm" style="width: 18rem">
 
-<div class="card-body d-flex flex-column align-items-center">
-  <h5 class="card-title">${tasks[a].title}</h5>
-  <p class="card-text text-center">
-    ${tasks[a].description}
-  </p>
-  <select class="form-select display-status"  aria-label="Default select example">
-        <option selected value="${tasks[a].status}" disabled>${tasks[a].status}</option>
-        <option onClick = "UpdateStatus(this.value, this.id)" id = "${a}" value="${unselected_options[0]}">${unselected_options[0]}</option>
-        <option onClick = "UpdateStatus(this.value, this.id)" id = "${a}" value="${unselected_options[1]}">${unselected_options[1]}</option>
-      </select>
-      <div class = "d-flex flex-row gap-5 m-4 justify-content-center">
-  <a href="#" onClick = "EditTask()" class="btn btn-warning">Edit</a>
-  <a href="#" onClick = "DeleteTask(this.id)" id = "${tasks[a].id}" class="btn btn-warning">Delete</a>
-  </div>
-  </div>
+
+<div class="card-body d-flex flex-column align-items-center justify-content-center">
+ <h5 class="card-title h-20">${tasks[a].title}</h5>
+ <p class="card-text text-center h-20">
+   ${tasks[a].description}
+ </p>
+ <select class="form-select h-20"
+   id="status-${tasks[a].id}"
+   onchange="UpdateStatus(this.value, '${tasks[a].id}')">
+   <option selected value="${tasks[a].status}" disabled>${tasks[a].status}</option>
+   <option value="${unselected_options[0]}">${unselected_options[0]}</option>
+   <option value="${unselected_options[1]}">${unselected_options[1]}</option>
+</select>
+     <div class = "d-flex flex-row gap-5 m-4 justify-content-end h-40">
+ <a href="#" onClick = "editTask(this.id)" id = "${tasks[a].id}" class="btn btn-warning">Edit</a>
+ <a href="#" onClick = "DeleteTask(this.id)" id = "${tasks[a].id}" class="btn btn-warning">Delete</a>
+ </div>
+ </div>
 </div> `;
   }
 
   if (tasks.length != 0) {
     all_tasks.innerHTML = htmlcode;
   } else {
-    all_tasks.innerHTML = `<b class = "m-4">No Tasks yet</b>`;
+    all_tasks.innerHTML = `<div class = "d-flex justify-content-center no-content flex-grow-1 ms-3 me-3"><b class="mt-2 mb-2">No Tasks yet</b></div>`;
   }
 }
-showTasks();
+let tasks = JSON.parse(localStorage.getItem("tasks-array"));
+showTasks(tasks);
 
-function DeleteTask(ids) {
-  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+function getIndexFromId(ids, storedtasks) {
   let index = 0;
   for (let a in storedtasks) {
     if (storedtasks[a].id == ids) index = a;
   }
+  return index;
+}
+
+function DeleteTask(ids) {
+  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+  let index = getIndexFromId(ids, storedtasks);
   storedtasks.splice(index, 1);
   localStorage.setItem("tasks-array", JSON.stringify(storedtasks));
-  showTasks();
+  showTasks(storedtasks);
 }
 
-function UpdateStatus(status_name, index) {
+function UpdateStatus(status_name, taskId) {
   let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+  let taskIndex = storedtasks.findIndex((task) => task.id === taskId);
+  if (taskIndex === -1) return;
 
-  storedtasks[index].status = status_name;
-
+  storedtasks[taskIndex].status = status_name;
   localStorage.setItem("tasks-array", JSON.stringify(storedtasks));
-  showTasks();
+
+  showTasks(storedtasks);
 }
 
-function Showselectedtasks(task) {
+function handleStatusChange(optionId) {
+  console.log(optionId);
+  const searchContentTitle = document.getElementById("searching-title");
+  const searchContentDescription = document.getElementById("searching-des");
+  const searchContentTitleDes = document.getElementById("searching-title-des");
+  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+  if (
+    searchContentTitle.value == "" &&
+    searchContentDescription.value == "" &&
+    searchContentTitleDes.value == ""
+  ) {
+    if (optionId == "all-task") {
+      console.log("all task");
+      showTasks(storedtasks);
+    } else {
+      Showselectedtasks(optionId, storedtasks);
+    }
+  } else {
+    let searchedTasks = JSON.parse(localStorage.getItem("searched-array"));
+    if (optionId == "all-task") showTasks(searchedTasks);
+    else {
+      Showselectedtasks(optionId, searchedTasks);
+    }
+  }
+}
+
+function Showselectedtasks(taskId, storedtasks) {
   all_tasks.innerHTML = "";
 
-  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
   let showtasktitle =
-    task == "done"
+    taskId == "done"
       ? "Done"
-      : task == "in-progress"
+      : taskId == "in-progress"
       ? "In Progress"
-      : task == "todo"
+      : taskId == "todo"
       ? "ToDo"
       : "";
   if (showtasktitle == "") {
@@ -132,29 +176,30 @@ function Showselectedtasks(task) {
     }
     if (storedtasks[a].status == showtasktitle) {
       let color = colormap.get(storedtasks[a].status);
-      htmlcode += `<div class="card m-3 ${color}" style="width: 18rem">
+      htmlcode += `<div class="card m-3 ${color} shadow-sm" style="width: 18rem">
 
-      <div class="card-body d-flex flex-column align-items-center">
-        <h5 class="card-title">${storedtasks[a].title}</h5>
-        <p class="card-text text-center">
-          ${storedtasks[a].description}
-        </p>
-        <select class="form-select display-status"  aria-label="Default select example">
-              <option selected value="${storedtasks[a].status}" disabled>${storedtasks[a].status}</option>
-              <option onClick = "UpdateStatus(${this.value}, this.id)" id = "dfdf" value="${unselected_options[0]}">${unselected_options[0]}</option>
-              <option onClick = "UpdateStatus(${this.value}, this.id)" id = "dff" value="${unselected_options[1]}">${unselected_options[1]}</option>
-            </select>
-            <div class = "d-flex flex-row gap-5 m-4 justify-content-center">
-        <a href="#" onClick = "EditTask()" class="btn btn-warning">Edit</a>
-        <a href="#" onClick = "DeleteTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Delete</a>
-        </div>
-        </div>
-      </div> `;
+
+     <div class="card-body d-flex flex-column align-items-center">
+       <h5 class="card-title">${storedtasks[a].title}</h5>
+       <p class="card-text text-center">
+         ${storedtasks[a].description}
+       </p>
+       <select class="form-select display-status"  aria-label="Default select example">
+             <option selected value="${storedtasks[a].status}" disabled>${storedtasks[a].status}</option>
+             <option onClick = "UpdateStatus(${this.value}, this.id)" value="${unselected_options[0]}">${unselected_options[0]}</option>
+             <option onClick = "UpdateStatus(${this.value}, this.id)" value="${unselected_options[1]}">${unselected_options[1]}</option>
+           </select>
+           <div class = "d-flex flex-row gap-5 m-4 justify-content-center">
+       <a href="#" onClick = "editTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Edit</a>
+       <a href="#" onClick = "DeleteTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Delete</a>
+       </div>
+       </div>
+     </div> `;
     }
   }
 
   if (htmlcode == "") {
-    all_tasks.innerHTML = `No Tasks under ${showtasktitle}`;
+    all_tasks.innerHTML = `<div class = "d-flex justify-content-center no-content flex-grow-1 ms-3 me-3"><b class="mt-3 mb-3">No Tasks under ${showtasktitle}</b></div>`;
   } else {
     all_tasks.innerHTML = htmlcode;
   }
@@ -175,7 +220,10 @@ function Searchtasks(event, ref) {
       ? "description"
       : "";
   let htmlcode = "";
-
+  let searchedOutput = [];
+  if (!localStorage.getItem("searched-array")) {
+    localStorage.setItem("searched-array", JSON.stringify([]));
+  }
   for (let a in storedtasks) {
     let unselected_options = calculate_unselected(storedtasks[a].status);
 
@@ -187,31 +235,78 @@ function Searchtasks(event, ref) {
         (text == storedtasks[a]["title"] ||
           text == storedtasks[a]["description"]))
     ) {
+      searchedOutput.push(storedtasks[a]);
       console.log("match");
-      htmlcode += `<div class="card m-3 ${color}" style="width: 18rem">
+      htmlcode += `<div class="card m-3 ${color} shadow-sm" style="width: 18rem">
 
-      <div class="card-body d-flex flex-column align-items-center">
-        <h5 class="card-title">${storedtasks[a].title}</h5>
-        <p class="card-text text-center">
-          ${storedtasks[a].description}
-        </p>
-        <select class="form-select display-status"  aria-label="Default select example">
-              <option selected value="${storedtasks[a].status}" disabled>${storedtasks[a].status}</option>
-              <option onClick = "UpdateStatus(this.value, this.id)" id = "dfdf" value="${unselected_options[0]}">${unselected_options[0]}</option>
-              <option onClick = "UpdateStatus(this.value, this.id)" id = "dff" value="${unselected_options[1]}">${unselected_options[1]}</option>
-            </select>
-            <div class = "d-flex flex-row gap-5 m-4 justify-content-center">
-        <a href="#" onClick = "EditTask()" class="btn btn-warning">Edit</a>
-        <a href="#" onClick = "DeleteTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Delete</a>
-        </div>
-        </div>
-      </div> `;
+
+     <div class="card-body d-flex flex-column align-items-center">
+       <h5 class="card-title">${storedtasks[a].title}</h5>
+       <p class="card-text text-center">
+         ${storedtasks[a].description}
+       </p>
+       <select class="form-select display-status"  aria-label="Default select example">
+             <option selected value="${storedtasks[a].status}" disabled>${storedtasks[a].status}</option>
+             <option onClick = "UpdateStatus(this.value, this.id)" id = "dfdf" value="${unselected_options[0]}">${unselected_options[0]}</option>
+             <option onClick = "UpdateStatus(this.value, this.id)" id = "dff" value="${unselected_options[1]}">${unselected_options[1]}</option>
+           </select>
+           <div class = "d-flex flex-row gap-5 m-4 justify-content-center">
+       <a href="#" onClick = "editTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Edit</a>
+       <a href="#" onClick = "DeleteTask(this.id)" id = "${storedtasks[a].id}" class="btn btn-warning">Delete</a>
+       </div>
+       </div>
+     </div> `;
     }
   }
 
+  localStorage.setItem("searched-array", JSON.stringify(searchedOutput));
+
   if (htmlcode.length == 0) {
-    all_tasks.innerHTML = "<b>No tasks under this search</b>";
+    all_tasks.innerHTML = `<div class = "d-flex justify-content-center no-content flex-grow-1 ms-3 me-3"><b class="mt-3 mb-3">No tasks under this search</b></div>`;
   } else {
     all_tasks.innerHTML = htmlcode;
   }
+}
+
+function editTask(identifier) {
+  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+
+  let index = getIndexFromId(identifier, storedtasks);
+
+  let editTaskObj = storedtasks[index];
+
+  addTaskText.textContent = "Update Task";
+  addButton.onclick = function () {
+    editTaskContent(identifier);
+  };
+  title.value = editTaskObj["title"];
+  description.value = editTaskObj["description"];
+  status.value = editTaskObj["status"];
+  statusOption1.disabled = false;
+  statusOtion2.disabled = false;
+}
+
+function editTaskContent(identifier) {
+  console.log(identifier);
+
+  let storedtasks = JSON.parse(localStorage.getItem("tasks-array"));
+  let index = getIndexFromId(identifier, storedtasks);
+
+  storedtasks[index].title = title.value;
+  storedtasks[index].description = description.value;
+  storedtasks[index].status = status.value;
+
+  title.value = "";
+  description.value = "";
+  status.value = "ToDo";
+
+  addTaskText.textContent = "Add Task";
+  addButton.onclick = function () {
+    addTask();
+  };
+  statusOption1.disabled = true;
+  statusOtion2.disabled = true;
+
+  localStorage.setItem("tasks-array", JSON.stringify(storedtasks));
+  showTasks(storedtasks);
 }
